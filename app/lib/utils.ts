b3199fc673894d0ca4032059bd0cdb4f70870c49
Rudra Pandya <viralpandya079@gmail.com>
2025-07-24 20:45:17 +0530

    Resume feedback components

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,10 @@
+import type { ClassValue } from "clsx";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]){
+  return twMerge(clsx(inputs))
+}
 export function formatSize(bytes: number): string {
   if (bytes === 0) return '0 Bytes';
 
